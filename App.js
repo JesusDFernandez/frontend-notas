20e7b1a792936b05af6a7f4a4941c4ded24890bd
JesusDFernandez <78115281+JesusDFernandez@users.noreply.github.com>
2023-10-13 20:25:24 -0400

    Configuración general

---
+++ App.js
@@ -0,0 +1,81 @@
+import { View } from 'react-native';
+import Constants from 'expo-constants'
+import Main from './src/components/Main';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/components/Login';
+import Signup from './src/components/Signup';
+import Tabs from './src/components/Tab';
+
+import { AuthProvider } from './src/context/authContext';
+import { TaskProvider } from './src/context/tasksContext';
+import { CategoryProvider } from './src/context/categoriesContext';
+
+const Stack = createNativeStackNavigator();
+
+
+export default function App() {
+
+
+  return (
+    <AuthProvider>
+      <TaskProvider>
+        <CategoryProvider>
+
+          <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
+
+            <NavigationContainer>
+              <Stack.Navigator
+                screenOptions={{
+                  headerTransparent: true,
+                  headerTitle: '',
+                  headerStyle: { backgroundColor: "transparent" },
+                  headerTitleStyle: { color: "#red" },
+                }}
+              >
+
+                <Stack.Screen name="Main" component={Main}
+
+                />
+
+                <Stack.Screen name="Tabs" component={Tabs}
+                  options={{
+                    headerBackVisible: false,
+
+                  }}
+                />
+                <Stack.Screen name="Login" component={Login}
+                  options={{
+                    headerTransparent: true,
+                    headerTitle: '',
+                    headerStyle: { backgroundColor: "transparent" },
+                    headerTitleStyle: { color: "#ffffff" },
+
+
+                  }}
+
+
+                />
+                <Stack.Screen name="Signup" component={Signup}
+                  options={{
+                    headerTransparent: true,
+                    headerTitle: '',
+                    headerStyle: { backgroundColor: "transparent" },
+                    headerTitleStyle: { color: "#ffffff", },
+                  }}
+                />
+
+              </Stack.Navigator>
+            </NavigationContainer>
+
+          </View>
+
+        </CategoryProvider>
+      </TaskProvider>
+    </AuthProvider>
+
+  );
+}
+
+
+
---
+++ src/config.js
@@ -0,0 +1,5 @@
+import { BACKEND_URL } from "@env"
+console.log(BACKEND_URL);
+export const API_URL = BACKEND_URL;
+
+
