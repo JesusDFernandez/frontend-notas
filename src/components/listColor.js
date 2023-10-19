4c5906b58c0711d7fa0335cf88e60e953a2d9961
JesusDFernandez <78115281+JesusDFernandez@users.noreply.github.com>
2023-10-19 16:22:10 -0400

    listado de colores y iconos

---
+++ src/components/listColor.js
@@ -0,0 +1,13 @@
+export const ListColor = [
+
+    "#9E9E9E",// (Gris mate)
+    "#F5F5DC",// (Beige mate)
+    "#8B4513",// (Marrón mate)
+    "#556B2F",// (Verde mate)
+    "#4169E1",//(Azul mate)
+    "#FFC0CB",//(Rosa mate)
+    "#800080",// (Morado mate)
+    "#FFD700",//(Amarillo mate)
+    "#FFA500",//(Naranja mate)
+    "#FF0000",//(Rojo mate)
+]
